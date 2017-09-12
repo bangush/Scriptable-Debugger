a18d3dfc642bf43d4c3a6000cca1ca184777e7ba
Maree Fourie <MareeF@k2.com>
2017-09-12 14:04:03 +0200

    Add CodeSlice and Delta debug scripts

---
+++ Scripts/DeltaDebugScript.py
@@ -0,0 +1,26 @@
+MdbgCmd.SetLogFile('C:\\dbg\\Log.log', TestName.Empty, LoggingAction.Overwrite)
+
+ps = MdbgCmd.ListProcesses()
+calculator = [p for p in ps if p.ProcessName == 'Calculator']
+
+MdbgCmd.AttachToProcess(calculator[0]);
+MdbgCmd.SetBreakPoint("CalculatorForm.cs", 88);
+
+def onNext(locationState):
+    if locationState.FileName == "":
+        MdbgCmd.StepOut(None, onNext)
+    elif locationState.FileName == "CalculatorForm.cs" and locationState.LineNumber > 88:
+        MdbgCmd.Go(testBegin, onBreak)
+    else:
+        MdbgCmd.Print()
+        MdbgCmd.Step(None, onNext)
+
+def onBreak(locationState):
+    MdbgCmd.Print()
+    MdbgCmd.Step(None, onNext)
+    
+def testBegin():
+    MdbgCmd.TestBegin()
+    
+MdbgCmd.Go(testBegin, onBreak)
+#C:\debugger\Scriptable-Debugger\Scriptable-Debugger\Scripts\Script.py
